fc132a6e970a51725b6ddc485827a97bd8e65f36
Tiep Phan <tieppt12@gmail.com>
2020-03-31 20:32:17 +0700

    demo override default value

--- velocity-context/src/context/user-context.tsx
+++ velocity-context/src/context/user-context.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import { User } from "../models/user";
 
 export const UserContext = React.createContext<User>({
-  id: 'some-uuid',
-  avatarUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Mr._Smiley_Face.svg/240px-Mr._Smiley_Face.svg.png',
-  playlist: ['Microservices', 'TypeScript programming'],
-  username: 'John Doe',
+  id: '',
+  avatarUrl: '',
+  playlist: [],
+  username: '',
   searchHistory: [],
 });
